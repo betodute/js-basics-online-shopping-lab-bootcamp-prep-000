1e4e681ef447517e9b3dfee8862f1a860b3e0822
Beto Dute <betodute@gmail.com>
2019-04-18 00:20:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,21 +10,63 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+ cart.push({itemName: item,
+   itemPrice: price,
+ });
+ return `${item} has been added to your cart.`;
 }
+
 
 function viewCart() {
-  // write your code here
+if (cart.length === 0) {
+  return "Your shopping cart is empty.";
+} else if (cart.length === 1) {
+  return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+} else if (cart.length === 2) {
+  return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+} else {
+  var itemNameAndPrice = [];
+
+  for (var i = 0; i < cart.length-1; i++) {
+    itemNameAndPrice.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+ }
+ 
+} return `In your cart, you have ${itemNameAndPrice.join(", ")}, and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`;
 }
 
+
 function total() {
-  // write your code here
+  var priceArray = [];
+  var total = 0;
+  for (var p = 0; p < cart.length; p++) {
+    priceArray.push(cart[p].itemPrice) ;
+  } 
+  for (var i = 0; i < priceArray.length; i++) {
+    total += priceArray[i];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
+  var nameStorageArray = [];
+  for (var i = 0; i <cart.length; i++) {
+    nameStorageArray.push(cart[i].itemName);
+  }
+  for (var f = 0; f < nameStorageArray.length; f++)  {
+    if (nameStorageArray[f] === item)  {
+      nameStorageArray.splice(f, 1);
+      return nameStorageArray;
+  } else { return "That item is not in your cart.";
+  } 
+  } 
+  }
+  
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    return ``
+  }
 }
