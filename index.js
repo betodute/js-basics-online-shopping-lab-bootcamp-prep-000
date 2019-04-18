fe475ca52df5e840b7e5d92feb6acf13dc07690b
Beto Dute <betodute@gmail.com>
2019-04-18 00:59:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
   
 
 function placeOrder(cardNumber) {
-  var emptyCart[];
+  var emptyCart = [];
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
